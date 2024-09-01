11e2a24d549dcbf7ea2b1f6198658e895591ffb4
AshisRai503 <02240334.cst@rub.edu.bt>
2024-09-01 21:12:13 +0600

    https://github.com/AshisRai503/csf101-python_exercises.git

---
+++ Operators/Arithmetic.py
@@ -0,0 +1,10 @@
+a, b = 10, 5
+print(f"a = {a}, b= {b} ")
+
+print(f"Addition : {a+b}")
+print(f"Substraction : {a-b}")
+print(f"Multiplication : {a*b}")
+print(f"Division : {a/b}")
+print(f"Modulus : {a%b}")
+print(f"Exponentiation : {a**b}")
+print(f"Floor Division: {a//b}")
---
+++ Operators/Assignment.py
@@ -0,0 +1,16 @@
+x = 18
+print(f"Initial x : {x}")
+
+x += 2
+print(f"After x += 2 : {x}")
+
+x -= 10
+print(f"After x -= 10 : {x}")
+
+x *= 2
+print(f"After x *= 2 : {x}")
+
+x /= 5
+print(f"After x /= 5 : {x}")
+
+
